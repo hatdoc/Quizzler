46d02a41f522981b1222f8a16c663318496ed58a
jedpark <hatdoc@naver.com>
2021-07-26 15:27:36 +1000

    add question.dart class to make it easier

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -26,13 +27,14 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
-  ];
 
-  List<bool> answer = [false, true, true];
+  List<Question> questionBank = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
+    Question(q: 'A slug\'s blood is green.', a: true),
+  ];
 
   int questionNumber = 0;
 
@@ -48,7 +50,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -72,6 +74,12 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnwer = questionBank[questionNumber].questionAnswer;
+                if (correctAnwer == true) {
+                  print('user got it right!');
+                } else {
+                  print('user got it wrong!');
+                }
                 setState(
                   () {
                     Text('pressed');
@@ -96,6 +104,12 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnwer = questionBank[questionNumber].questionAnswer;
+                if (correctAnwer == false) {
+                  print('user got it right!');
+                } else {
+                  print('user got it wrong!');
+                }
                 setState(
                   () {
                     Text('pressed');
